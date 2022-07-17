c61980dc2eb18c1cdd034de2fcc941f9dd00609e
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-07-17 20:43:16 +0700

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -1,4 +1,5 @@
 const error = document.querySelector(".error");
+const error2 = document.querySelector(".error2");
 const input = document.querySelector("input");
 
 //
@@ -8,7 +9,8 @@ function validate() {
 
   if (mail.match(mailformat)) {
     return true;
-  } else {
+  } else if (mail === "") return 1;
+  else {
     return false;
   }
 }
@@ -17,9 +19,18 @@ send.addEventListener("click", (e) => {
   e.preventDefault();
   if (validate() === true) {
     error.parentNode.classList.remove("active");
-    alert("Thank you, we will come back to you soon");
+    error2.parentNode.classList.remove("hide");
+    input.style.border = "2px solid rgb(7, 163, 7)";
+    thanks.parentNode.classList.add("display");
+  } else if (validate() === 1) {
+    error.parentNode.classList.remove("active");
+    thanks.parentNode.classList.remove("display");
+    error2.parentNode.classList.add("hide");
+    input.style.border = "2px solid red";
   } else {
+    input.style.border = "2px solid red";
     error.parentNode.classList.add("active");
-    input.parentNode.classList.add("active");
+    error2.parentNode.classList.remove("hide");
+    thanks.parentNode.classList.remove("display");
   }
 });
